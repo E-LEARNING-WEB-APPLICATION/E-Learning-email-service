b29ffd4eff21e2c219391c4177145c3791d6474b
gandhiomkar <gandhioms16@gmail.com>
2026-01-29 14:52:38 +0530

    fix: removed useHttpRedirection to https because java doesnt have https certificate.
    this is temporary fix for development
    later get certificatef from dotnet and add it to java keyring for production

--- EmailService/Program.cs
+++ EmailService/Program.cs
@@ -35,7 +35,7 @@ var app = builder.Build();
 //    app.MapOpenApi();
 //}
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
